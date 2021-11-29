610cbdf688f254adec6bab5a98059f383ef3be30
dotieuthien <dotieuthien9997@gmail.com>
2021-11-29 20:08:32 +0700

    Remove *.pyc files

---
+++ tests/imshow.py
@@ -0,0 +1,11 @@
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+img1 = cv2.imread('/home/hades/Desktop/Tool/data/05_ggg05_s02_010_r2S_T_A_CHS_NON/color/annotations/0006.png')
+img2 = cv2.imread('/home/hades/Desktop/Tool/data/05_ggg05_s02_010_r2S_T_A_CHS_NON/color/annotations/0007.png')
+print(np.unique(img1))
+img = np.hstack((img1, img2))
+plt.imshow(img)
+plt.show()
